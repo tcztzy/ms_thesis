38e5321bfc9d696f8f386f876d57a0fdd92ed403
Tang Ziya <tcztzy@gmail.com>
2022-03-24 18:46:41 +0800

    ADD: meteorology data

--- plots.py
+++ plots.py
@@ -1,14 +1,26 @@
+#!/home/tcztzy/.pyenv/versions/thesis/bin/python
+import argparse
+import datetime
 import pathlib
+import tarfile
+from typing import TYPE_CHECKING
+
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import tarfile
+import pint
+import pint_pandas
+import seaborn as sns
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
 from datav import DataVGeoAtlasFeature
+from matplotlib import cm
 from matplotlib.font_manager import FontProperties
 from matplotlib.transforms import offset_copy
+
+if TYPE_CHECKING:
+    from typing import Generator
 
 SimSun = FontProperties(fname="/mnt/c/Windows/Fonts/simsun.ttc")
 
@@ -75,8 +87,8 @@ def research_site(fontproperties: FontProperties, path, dpi=800):
     plt.savefig(path, dpi=dpi)
 
 
-def alar_gsod():
-    def g():
+def alar_gsod() -> pd.DataFrame:
+    def g() -> "Generator[pd.DataFrame]":
         for year in (2019, 2020, 2021):
             with tarfile.open(f"{year}.tar.gz", "r:gz") as tar:
                 yield pd.read_csv(
@@ -89,12 +101,133 @@ def alar_gsod():
     return pd.concat(g())
 
 
+def f2c(d):
+    return (d - 32) / 1.8
+
+
+def plot_climate_one_year(data, ax, fontproperties=SimSun):
+    twin1 = ax.twinx()
+    twin2 = ax.twinx()
+    twin2.spines["right"].set_position(("axes", 1.05))
+    ax.set_ylim(0, 45)
+    twin1.set_ylim(-5, 40)
+    twin2.set_ylim(0, 20)
+    (p1,) = ax.plot(
+        data["DOY"],
+        data["ALLSKY_SFC_SW_DWN"],
+        color="gray",
+        linestyle="--",
+        label="SRAD",
+    )
+    p2_color = cm.hot(0.3)
+
+    sns.regplot(
+        x="DOY",
+        y="MAX",
+        line_kws={"color": p2_color},
+        scatter_kws={"color": p2_color},
+        data=data,
+        ax=twin1,
+        lowess=True,
+        label="TMAX",
+    )
+    p2 = twin1.get_children()[0]
+    p3_color = cm.hot(0.0)
+    sns.regplot(
+        x="DOY",
+        y="MIN",
+        line_kws={"color": p3_color},
+        scatter_kws={"color": p3_color},
+        data=data,
+        ax=twin1,
+        lowess=True,
+        label="TMIN",
+    )
+    p3 = twin1.get_children()[1]
+    p4 = twin2.bar(
+        data["DOY"],
+        data["PRCP"],
+        color="deepskyblue",
+        label="RAIN",
+    )
+
+    ax.set_ylabel("太阳辐射 ($\mathrm{MJ\ m^{-3} d^{-1}}$)", fontproperties=fontproperties)
+    twin1.set_ylabel("气温 ($\mathrm{C^{\circ}}$)", fontproperties=fontproperties)
+    twin2.set_ylabel("降水 (mm)", fontproperties=fontproperties)
+    ax.yaxis.label.set_color(p1.get_color())
+    twin1.yaxis.label.set_color(p2_color)
+    twin2.yaxis.label.set_color("deepskyblue")
+
+    tkw = dict(size=4, width=1.5)
+    ax.tick_params(axis="y", colors=p1.get_color(), **tkw)
+    # twin1.tick_params(axis="y", colors=p2_color, **tkw)
+
+    yticklabels = twin1.get_yticklabels()
+    for ticklabel, tickcolor in zip(
+        yticklabels,
+        cm.hot(np.linspace(0.0, 0.3, len(yticklabels))),
+    ):
+        ticklabel.set_color(tickcolor)
+    twin2.tick_params(axis="y", colors="deepskyblue", **tkw)
+    ax.tick_params(axis="x", **tkw)
+    ax.legend(handles=[p1, p2, p3, p4])
+
+
+def plot_climate(data, path="climate.png"):
+    _, axes = plt.subplots(3, 1, figsize=(12, 15), dpi=800)
+
+    for year, ax in zip((2019, 2020, 2021), axes):
+        plot_climate_one_year(data.loc[f"{year}-04-01":f"{year}-11-01"], ax)
+        if year < 2021:
+            ax.tick_params(
+                axis="x",
+                which="both",  # both major and minor ticks are affected
+                bottom=False,  # ticks along the bottom edge are off
+                top=False,  # ticks along the top edge are off
+                labelbottom=False,
+            )
+    axes[2].set_xlabel("DOY")
+    plt.subplots_adjust(hspace=0.1)
+
+    plt.savefig(path)
+
+
 if __name__ == "__main__":
-    if not (research_site_path := pathlib.Path("research_site.png")).exists():
+    pint_pandas.PintType.ureg.setup_matplotlib()
+    ureg = pint.UnitRegistry()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--force", default=False, action="store_true")
+    args = parser.parse_args()
+    force: bool = args.force
+    if not (research_site_path := pathlib.Path("research_site.png")).exists() or force:
         research_site(fontproperties=SimSun, path=research_site_path)
-    if not (gsod_path := pathlib.Path("alar.gsod.csv")).exists():
-        alar_climate = alar_gsod()
-        alar_climate.to_csv(gsod_path)
+    if not (alar_climate_path := pathlib.Path("alar.climate.pkl")).exists() or force:
+        power = pd.read_csv(
+            "POWER_Point_Daily_20190101_20220319_040d6240N_081d1960E_LST.csv",
+            skiprows=19,
+        )
+        power.index = power["YEAR"].apply(lambda y: datetime.date(y, 1, 1)) + power[
+            "DOY"
+        ].apply(lambda doy: datetime.timedelta(days=doy - 1))
+        alar_climate = alar_gsod().merge(power, left_index=True, right_index=True)
+        alar_climate.to_pickle(alar_climate_path)
     else:
-        alar_climate = pd.read_csv(gsod_path, index_col=0, parse_dates=[0])
-    print(alar_climate)
+        alar_climate = pd.read_pickle(alar_climate_path)
+    alar_climate = alar_climate.astype(
+        {
+            "MAX": "pint[degF]",
+            "MIN": "pint[degF]",
+            "WDSP": "pint[knot]",
+            "PRCP": "pint[inch]",
+        }
+    )
+    for col, to in {
+        "MAX": "degC",
+        "MIN": "degC",
+        "WDSP": "km/d",
+        "PRCP": "mm",
+    }.items():
+        alar_climate[col] = alar_climate[col].pint.to(to)
+    alar_climate.loc[alar_climate["PRCP"] == 9.906 * ureg.mm, "PRCP"] = np.nan
+    if not (climate_path := pathlib.Path("climate.png")).exists() or force:
+        plot_climate(alar_climate, climate_path)
